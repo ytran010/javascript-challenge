6ab0e4b6726e2e727cda50e50e4c33d92804cb4c
Yvonne Tran <petertran@Yvonnes-MacBook-Pro.local>
2021-03-21 19:32:20 -0700

    Populated tables and filter search

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,54 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+
+
+var tbody = d3.select("tbody");
+data.forEach((UFOSighting) => {
+    var row = tbody.append("tr");
+    Object.entries(UFOSighting).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+});
+
+var form = d3.select("form");
+
+form.on("submit",runEnter);
+// var inputField = d3.select("#form-control");
+// inputField.on("change", function() {
+//     d3.event.preventDefault();
+//     var newText = d3.event.target.value;
+//     console.log(newText);
+    
+//   });
+
+  function runEnter() {
+
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+    
+    // Select the input element and get the raw HTML node
+    var inputElement = d3.select("#datetime");
+  
+    // Get the value property of the input element
+    var inputValue = inputElement.property("value");
+  
+    // Print the value to the console
+    console.log(inputValue);
+    
+    var filteredData = data.filter(ufo => ufo.datetime == inputValue);
+    console.log(filteredData);
+    // Clear out table
+    tbody.html("");
+    filteredData.forEach((UFOSighting) => {
+        var row = tbody.append("tr");
+        Object.entries(UFOSighting).forEach(([key, value]) => {
+          var cell = row.append("td");
+          cell.text(value);
+        });
+    });
+
+  }
+  
+  
